d0cc624a24a4bfa776f215a8832a7b644743d598
Gabriel Francisco Bravo <bielbravo2016@gmail.com>
2025-06-09 16:14:54 -0300

    Update ex003(c).py

--- LISTA_1_PROG1/ex003(c).py
+++ LISTA_1_PROG1/ex003(c).py
@@ -2,4 +2,10 @@ cor = input("Digite a cor aqui:\n")]
 cor = cor.upper()
 if cor == "VERDE":
   print("O preço é 10 reais")
+elif cor == "AZUL":
+  print("O preço é 20 reais")
+elif cor == "AMARELO":
+  print("O preço é 30 reais")
+elif cor == "VERMELHO":
+  print("O preço é 40 reais")
   
